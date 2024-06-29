a6e8cbf1fe026aca150f70d57e712f021c14fe35
isabellacnuedu <isabella.do.22@cnu.edu>
2024-06-29 19:08:42 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,110 @@
+import ollama
+import streamlit as st
+import os
+import json
+import numpy as np
+from numpy.linalg import norm
+
+def parse_file(filename):
+    with open(filename, encoding="utf-8-sig") as f:
+        paragraphs = []
+        buffer = []
+        for line in f.readlines():
+            line = line.strip()
+            if line: 
+                buffer.append(line)
+            elif len(buffer):
+                paragraphs.append((" ").join(buffer))
+                buffer = []
+        if len(buffer):
+            paragraphs.append((" ").join(buffer))
+        return paragraphs
+    
+
+def get_embeddings(filename, modelname, chunks):
+    if (embeddings := load_embeddings(filename)) is not False:
+        return embeddings
+    embeddings = [
+        ollama.embeddings(model=modelname, prompt=chunk)["embedding"]
+        for chunk in chunks
+    ]
+    save_embeddings(filename, embeddings)
+    return embeddings
+
+def load_embeddings(filename):
+    if not os.path.exists(f"embeddings/{filename}.json"):
+        return False
+    with open(f"embeddings/{filename}.json", "r") as f:
+        return json.load(f)
+
+def save_embeddings(filename, embeddings):
+    if not os.path.exists("embeddings"):
+        os.makedirs("embeddings")
+    with open(f"embeddings/{filename}.json", "w") as f:
+        json.dump(embeddings, f)
+
+def find_most_similar(needle, haystack):
+    needle_norm = norm(needle)
+    similarity_scores = [
+        np.dot(needle, item) / (needle_norm * norm(item)) for item in haystack
+    ]
+    return sorted(zip(similarity_scores, range(len(haystack))), reverse=True)
+
+def get_response(prompt, filename="Computer Science_Apr24.txt", model="phi"):
+    SYSTEM_PROMPT = """You are a helpful Christopher Newport University student advisor who answers questions 
+        based on snippets of text provided in context. Answer only using the context provided, 
+        being as concise as possible. If you're unsure, just say that you don't know.
+        Context:
+    """
+    paragraphs = parse_file(filename)
+    embeddings = get_embeddings(filename, model, paragraphs)
+    prompt_embedding = ollama.embeddings(model=model, prompt=prompt)["embedding"]
+    most_similar_chunks = find_most_similar(prompt_embedding, embeddings)[:5]
+
+    response = ollama.chat(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+                + "\n".join(paragraphs[item[1]] for item in most_similar_chunks),
+            },
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response["message"]["content"]
+
+st.title("Ollama Python Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+if "model" not in st.session_state:
+    st.session_state["model"] = ""
+
+models = [model["name"] for model in ollama.list()["models"]]
+st.session_state["model"] = st.selectbox("Choose your model", models)
+
+def model_res_generator():
+    stream = ollama.chat(
+        model=st.session_state["model"],
+        messages=st.session_state["messages"],
+        stream=True,
+    )
+    for chunk in stream:
+        yield chunk["message"]["content"]
+
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = get_response(prompt, model=st.session_state["model"])
+        st.markdown(response)
+        st.session_state["messages"].append({"role": "assistant", "content": response})
